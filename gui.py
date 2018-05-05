c0cb1a8aaa1fd9f3eb47ebd82cd77adeb3f6a61d
Yoshi哟喜 <38394135+yosiyoshi@users.noreply.github.com>
2018-05-06 00:20:21 +0900

    Update gui.py

--- gui.py
+++ gui.py
@@ -119,6 +119,78 @@ text = tk.Label(text=txt)
 text.pack()
 
 root.mainloop()
+                    m[w+1, h+1] = 1
+                    m[w+2, h+2] = 1
+                w = w + 1
+        h = h + 1
+        a = 0
+        while a <= 3:
+            if m[a, 0] == 1:
+                if m[a, 2] == 1:
+                    m[a, 1] = 1
+            elif m[a, 1] == 1:
+                if m[a, 3] == 1:
+                    m[a, 2] = 1
+            a = a + 1
+    w = random.randint(0, 2)
+    h = random.randint(0, 2)
+    while h <= 1:
+        if m[w-1, h] + m[w, h] == 0:
+            if m[w, h] == 0:
+                m[w, h] = -1
+                w = w + 1
+                if 1 <= w <=2:
+                    if m[w-1, h] == m[w+1, h]:
+                        m[w-1, h] = -1
+                        m[w+1, h] = -1
+                elif w == 1:
+                    if m[w, h] == m[w+2, h]:
+                        m[w+1, h] = -1
+                        m[w+2, h] = -1
+                elif w >= 2:
+                    if m[w, h] == m[w-2, h]:
+                        m[w-1, h] = -1
+                        m[w-1, h] = -1
+            elif m[w, h] == -1:
+                if 0 <= w <= 1:
+                    m[w+1, h] = -1
+                    m[w+2, h] = -1
+                w = w + 1
+        elif m[w, h] + m[w, h+1] == 0:
+            if m[w, h] == 0:
+                w = w + 1
+            elif m[w, h] == -1:
+                if w <= 1:
+                    m[w, h+1] = -1
+                    m[w, h+2] = -1
+                w = w + 1
+        elif w <= 2:
+            if m[w, h] + m[w+1, h+1] == 0:
+                if m[w, h] == 0:
+                    w = w + 1
+                elif m[w, h] == -1:
+                    if w <= 1:
+                        m[w+1, h+1] = -1
+                        m[w+2, h+2] = -1
+                    w = w + 1
+        h = h + 1
+        a = 0
+        while a < 3:
+            if m[a, 0] == -1:
+                if m[a, 2] == -1:
+                    m[a, 1] = -1
+            elif m[a, 1] == -1:
+                if m[a, 3] == -1:
+                    m[a, 2] = -1
+            a = a + 1
+    t = t + 1
+root = tk.Tk()
+txt = 'turns:\n' + str(end) + '\n' + 'result:\n' + str(m)
+root.title('result')
+text = tk.Label(text=txt)
+text.pack()
+
+root.mainloop()
                 w = w + 1
         h = h + 1
         a = 0
