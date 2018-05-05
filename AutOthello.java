158e684adb5e4522fe127bfdabb4b2605baaa499
Yoshi哟喜 <38394135+yosiyoshi@users.noreply.github.com>
2018-05-05 22:27:11 +0900

    Update AutOthello.java

--- AutOthello.java
+++ AutOthello.java
@@ -11,15 +11,14 @@ public class AutOthello{
 		int w = rand.nextInt(3);
 		int h = rand.nextInt(3);
 		while (h <= 1) {
-    		if (m[w-1][h]+m[w][h]>=-1); {
+    		if (m[w-1][h]+m[w][h]>=-1) {
              m[w][h] = 0;
              w = w + 1;
              if (1 <= w && w <= 2); {
                  m[w-1][h] = 1;
                  m[w+1][h] = 1;
-// Here has an error in compiling. 
             } else if (w <= 1) {
-                 if (m[w][h] == m[w+2][h]); {
+                 if (m[w][h] == m[w+2][h]) {
                  m[w+1][h] = 1;
                  m[w+2][h] = 1;
                      }
